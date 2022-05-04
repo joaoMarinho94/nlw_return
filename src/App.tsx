e8d21aaa2567d27790f0fcdd9e709ed7f1ec710f
joaoMarinho94 <joaopaulim94@gmail.com>
2022-05-03 21:46:18 -0300

    feat: add component Widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
+import { Widget } from 'components/Widget';
 import React from 'react';
 
-function App() {
-  return <h1 className="bg-violet-500 p-3">hello word</h1>;
+export function App() {
+  return <Widget />;
 }
-
-export default App;
