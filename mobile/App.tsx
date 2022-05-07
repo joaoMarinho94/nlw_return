23611381ef2d1acc4f8c005dcc8546ffe183f436
joaoMarinho94 <joaopaulim94@gmail.com>
2022-05-07 20:57:36 -0300

    feat: add eslint, prettier and editorconfig

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
+
+import React from 'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello word!</Text>
       <StatusBar style="auto" />
-
-
-      
     </View>
   );
 }
